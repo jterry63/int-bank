688ff9ae4cd9ec9d1fd42c7b69a3a9ff5c1d19a9
jterry <jordan.terry@mx.com>
2020-08-03 10:37:42 -0600

    updated db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,7 @@ var xml = require('xml');
 
 router.get("/db/user", function (req, res) {
   
-  var dbQuery = `SELECT birthdate, credit_score, email, first_name, gender, user_id, last_name, metadata, phone, zip_code FROM freedb_intbank3f7a4412d234dgdsfusers WHERE user_id='${req.query.user_id}'`;
+  var dbQuery = `SELECT birthdate, credit_score, email, first_name, gender, user_id, last_name, metadata, phone, zip_code FROM freedb_intbank3f7a4412d234dgdsf.users WHERE user_id='${req.query.user_id}'`;
 
   connection.query(dbQuery, function (err, result) {
     if (err) throw err;
@@ -23,7 +23,7 @@ router.get("/db/user", function (req, res) {
 
 router.post("/db/users", function (req, res) {
 
-  var dbQuery = "INSERT INTO freedb_intbank3f7a4412d234dgdsfusers (birthdate, credit_score, email, first_name, gender, user_id, last_name, metadata, phone, zip_code) VALUES (?,?,?,?,?,?,?,?,?,?)";
+  var dbQuery = "INSERT INTO freedb_intbank3f7a4412d234dgdsf.users (birthdate, credit_score, email, first_name, gender, user_id, last_name, metadata, phone, zip_code) VALUES (?,?,?,?,?,?,?,?,?,?)";
 
   connection.query(dbQuery, [req.query.birthdate, req.query.credit_score, req.query.email, req.query.first_name, req.query.gender, req.query.user_id, req.query.last_name, req.query.metadata, req.query.phone, req.query.zip_code], function (err, result) {
     if (err) throw err;
@@ -36,7 +36,7 @@ router.post("/db/users", function (req, res) {
 
 router.post("/db/accounts", function (req, res) {
 
-  var dbQuery = "INSERT INTO freedb_intbank3f7a4412d234dgdsfaccounts (account_type, account_name, balance, available_balance, account_number, currency_code, account_id, user_id) VALUES (?,?,?,?,?,?,?,?)";
+  var dbQuery = "INSERT INTO freedb_intbank3f7a4412d234dgdsf.accounts (account_type, account_name, balance, available_balance, account_number, currency_code, account_id, user_id) VALUES (?,?,?,?,?,?,?,?)";
 
   connection.query(dbQuery, [req.query.account_type, req.query.account_name, req.query.balance, req.query.available_balance, req.query.account_number, req.query.currency_code, req.query.account_id, req.query.user_id], function (err, result) {
     if (err) throw err;
@@ -49,7 +49,7 @@ router.post("/db/accounts", function (req, res) {
 
 router.post("/db/transactions", function (req, res) {
 
-  var dbQuery = "INSERT INTO freedb_intbank3f7a4412d234dgdsftransactions (transaction_id, transaction_status, transaction_type, amount, transaction_description, transacted_on, posted_on, category, user_id, account_id) VALUES (?,?,?,?,?,?,?,?,?,?)";
+  var dbQuery = "INSERT INTO freedb_intbank3f7a4412d234dgdsf.transactions (transaction_id, transaction_status, transaction_type, amount, transaction_description, transacted_on, posted_on, category, user_id, account_id) VALUES (?,?,?,?,?,?,?,?,?,?)";
 
   connection.query(dbQuery, [req.query.transaction_id, req.query.transaction_status, req.query.transaction_type, req.query.amount, req.query.transaction_description, req.query.transacted_on, req.query.posted_on, req.query.category, req.query.user_id, req.query.account_id,], function (err, result) {
     if (err) throw err;
@@ -67,7 +67,7 @@ router.post("/db/transactions", function (req, res) {
 
 router.post("/int-bank/sessions", function (req, res) {
   
-  var dbQuery = `SELECT user_id FROM freedb_intbank3f7a4412d234dgdsfusers WHERE user_id='${req.body.mdx.session[0].userkey[0]}'`;
+  var dbQuery = `SELECT user_id FROM freedb_intbank3f7a4412d234dgdsf.users WHERE user_id='${req.body.mdx.session[0].userkey[0]}'`;
 
   connection.query(dbQuery, function (err, result) {
     if (err) throw err;
@@ -82,7 +82,7 @@ router.post("/int-bank/sessions", function (req, res) {
 
 router.get("/int-bank/accounts", function (req, res) {
   
-  var dbQuery = `SELECT account_type, account_name, balance, available_balance, account_number, currency_code, account_id, user_id FROM freedb_intbank3f7a4412d234dgdsfaccounts WHERE user_id='${req.header('MDX-Session-Key')}'`;
+  var dbQuery = `SELECT account_type, account_name, balance, available_balance, account_number, currency_code, account_id, user_id FROM freedb_intbank3f7a4412d234dgdsf.accounts WHERE user_id='${req.header('MDX-Session-Key')}'`;
 
   connection.query(dbQuery, function (err, result) {
     if (err) throw err;
@@ -97,7 +97,7 @@ router.get("/int-bank/accounts", function (req, res) {
 
 router.get("/int-bank/accounts/:account_id/transactions", function (req, res) {
   
-  var dbQuery = `SELECT transaction_id, transaction_status, transaction_type, amount, transaction_description, transacted_on, posted_on, category, user_id, account_id FROM freedb_intbank3f7a4412d234dgdsftransactions WHERE account_id='${req.params.account_id}'`;
+  var dbQuery = `SELECT transaction_id, transaction_status, transaction_type, amount, transaction_description, transacted_on, posted_on, category, user_id, account_id FROM freedb_intbank3f7a4412d234dgdsf.transactions WHERE account_id='${req.params.account_id}'`;
 
   connection.query(dbQuery, function (err, result) {
     if (err) throw err;
